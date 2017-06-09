82156c47e88ab23365b2fe4ce212af2f3e22ecc6
Jean-Baptiste Noblot <noblot.jb@gmail.com>
2017-06-09 17:11:39 +0200

    Add Dat.GUI

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,83 @@
 "use strict";
+import * as dat from "dat-gui"; 
 
-const canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("canvas_gol");
-let ctx:CanvasRenderingContext2D|null = null;
-if (canvas === null || !canvas.getContext)
-{
-    console.error("Init Fail");
-    stop();
-}
-else
-{
 
-    ctx = canvas.getContext("2d");
-    if (ctx === null)
+class CanvasParams
+{
+    public readonly canvas: HTMLCanvasElement|null;
+    public xCanvas: number;
+    public yCanvas: number;
+    public squareWidth = 5;
+    public ctx:CanvasRenderingContext2D|null = null;
+
+    public constructor()
     {
-        console.error("Init Fail getContext 2D");
+        this.canvas = <HTMLCanvasElement>document.getElementById("canvas_gol");
+    }
+
+
+    public nextStep()
+    {
+
+    }
+
+    public play(){
+        
+    }
+}
+
+let canvasParams:CanvasParams;
+let gui:dat.GUI;
+
+window.onload = function() {
+    canvasParams = new CanvasParams();
+    gui = new dat.GUI();
+
+    let ctx:CanvasRenderingContext2D|null = null;
+    if (canvasParams.canvas === null || !canvasParams.canvas.getContext)
+    {
+        console.error("Init Fail");
+        stop();
     }
     else
     {
-        canvas.addEventListener("pointerdown", pointerDownEvent);
-        document.addEventListener("pointerup", pointerUpEvent);
+        ctx = canvasParams.canvas.getContext("2d");
+        if (ctx === null)
+        {
+            console.error("Init Fail getContext 2D");
+        }
+        else
+        {
+            canvasParams.xCanvas = canvasParams.canvas.offsetTop;
+            canvasParams.yCanvas = canvasParams.canvas.offsetLeft;
+            canvasParams.canvas.addEventListener("pointerdown", pointerDownEvent);
+            document.addEventListener("pointerup", pointerUpEvent);
+        }
     }
+
+    gui.add(canvasParams, 'squareWidth');
+
 }
+
+
+
 
 function pointerDownEvent(ev:PointerEvent): any
 {
-    console.log(ev);
-    canvas.addEventListener("pointermove", pointerMoveEvent);
+    canvasParams.canvas.addEventListener("pointermove", pointerMoveEvent);
 }
 
 function pointerUpEvent(ev:PointerEvent): any
 {
-    console.log("UP "+ ev);
-    canvas.removeEventListener("pointermove", pointerMoveEvent);
+    canvasParams.canvas.removeEventListener("pointermove", pointerMoveEvent);
 }
 
 function pointerMoveEvent(ev:PointerEvent): any
 {
     if (ctx !== null)
     {
-        console.log(ev);
         ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect (ev.x, ev.y, 1, 1);
+        ctx.fillRect (ev.x, ev.y, squareWidth, squareWidth);
     }
 }
 
